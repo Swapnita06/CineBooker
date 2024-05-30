9b0a8844d604090b1ccc8c2bf41f19cb95db5485
Swapnita Singh <swapimamuni@gmail.com>
2024-05-31 00:42:20 +0530

    user

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -20,17 +20,22 @@ export const getAllUsers = async(req,res,next)=>{
 
 export const signup= async(req,res,next)=>{
     const { name, email,password}= req.body;
-    if(!name|| name.trim()===""|| !email.trim()===""|| !password || password.trim()==="")
+    if(
+        !name|| 
+        name.trim()===""|| 
+        !email.trim()===""|| 
+        !password || password.trim()===""
+    )
 {
     return res.status(422).json({message: "Invalid Inputs"});
 }
 
-const newPassword = bcrypt.hashSync(password);
+const hashedPassword = bcrypt.hashSync(password);
 
 let user;
 try{
 
-    user = new User({name, email,password});
+    user = new User({name, email,password: hashedPassword});
     user = await user.save();
 
 }
@@ -42,3 +47,67 @@ if(!user){
 }
  return res.status(201).json({user});
     };
+
+    export const updateUser = async(req,res,next)=>{
+        const id = req.params.id;
+        const { name, email,password}= req.body;
+        if(!name|| name.trim()===""|| !email.trim()===""|| !password || password.trim()==="")
+    {
+        return res.status(422).json({message: "Invalid Inputs"});
+    }
+
+    
+    const hashedPassword = bcrypt.hashSync(password);
+
+    let user;
+    try{
+        user = await User.findByIdAndUpdate(id,{name,email,password:hashedPassword});
+    }catch(err){
+return console.log(err);
+    }
+    if(!user){
+        return res.status(500).json({message:"Unexpected Error Occured"});
+        }
+        return res.status(200).json({message:"Updated Successfully"});
+};
+
+export const deleteUser= async(req,res,next)=>{
+    const id = req.params.id;
+    let user;
+    try{
+        user = await User.findByIdAndDelete(id);
+        }catch(err){
+            return console.log(err);
+            }
+            if(!user){
+                return res.status(500).json({message:"Unexpected Error Occured"});
+                }
+                return res.status(200).json({message:"Deleted Successfully"});
+};
+
+
+export const login = async(req,res,next)=>{
+    const {email,password}= req.body;
+    if(!email.trim()===""|| !password || password.trim()==="")
+        {
+            return res.status(422).json({message: "Invalid Inputs"});
+            }
+
+            let existingUser;
+          try{
+            existingUser = await User.findOne({email});
+}  
+catch(err){
+    return console.log(err);
+    }
+
+    if(!existingUser){
+        return res.status(400).json({message:"User not Found"});
+        }
+
+    const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid Credentials"});
+        }
+        return res.status(200).json({message:"Login Successful"});
+}
