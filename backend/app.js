96f3b714330b60fb04a773ba8845ba4748a3c9be
Swapnita Singh <swapimamuni@gmail.com>
2024-05-31 21:57:40 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRouter from "./routes/user_routes.js";
+import adminRouter from "./routes/admin_routes.js";
+import movieRouter from "./routes/movie_routes.js";
 dotenv.config();
 const app= express();
 
 app.use(express.json());
 app.use("/user",userRouter);
-
+app.use("/admin",adminRouter);
+app.use("/movie",movieRouter)
 
 mongoose.connect(
     `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.j6rlymu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
