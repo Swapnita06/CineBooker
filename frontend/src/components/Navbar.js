4bfb17c33fa9fa6e57d9ca346c099b052f1fa03c
Swapnita Singh <swapimamuni@gmail.com>
2024-06-04 19:02:55 +0530

    change

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AppBar, Autocomplete, Box, Tab, Tabs, TextField, Toolbar } from '@mui/material'
 import MovieCreationIcon from '@mui/icons-material/MovieCreation';
+import { getAllMovies } from '../api-helpers/api-helpers';
 
 const dummyArray=["movie1"," movie2", "movie3"]
 const Navbar = () => {
  const [value,setValue]= useState(0);
+
+ const[movies,setMovies]= useState([])
+ useEffect(()=>{
+    getAllMovies()
+    .then((data)=>setMovies(data.movies))
+    .catch((err)=> console.log(err));
+ },[])
   return (
 <AppBar sx ={{bgcolor:"#070F2B"}}>
     <Toolbar>
@@ -14,7 +22,7 @@ const Navbar = () => {
         <Box width={'30%'} margin={"auto"}>
         <Autocomplete
         freeSolo
-        options={dummyArray.map((option) => option)}
+        options={movies && movies.map((option) => option.title)}
         renderInput={(params) =>(
              <TextField sx ={{input:{color:"white"} }}
              variant='standard' {...params} 
@@ -22,7 +30,7 @@ const Navbar = () => {
       />
         </Box>
         <Box display={'flex'}>
-<Tabs textColor='inherit' indicatorColor='secondary' value={value} onChange={(e,val)=>setValue(val)}>
+<Tabs textColor='inherit' indicatorColor='white' value={value} onChange={(e,val)=>setValue(val)}>
 <Tab label="Movies"/>
     <Tab label="Admin"/>
     <Tab label="Auth"/>
