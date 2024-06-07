665181d61b35d7536d840a3ebe7819f478ec3493
Swapnita Singh <swapimamuni@gmail.com>
2024-06-08 01:20:23 +0530

    moviepage

--- frontend/src/pages/Movies.js
+++ frontend/src/pages/Movies.js
@@ -1,10 +1,30 @@
 import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllMovies } from '../api-helpers/api-helpers';
+import MovieItem from '../components/Movies/MovieItem';
 
 const Movies = () => {
+  const [movies,setMovies] = useState();
+  useEffect(()=>{
+ getAllMovies().then((data)=>setMovies(data.movies))
+ .catch((err)=>console.log(err));
+  },[]);
   return (
     <Box margin={'auto'} marginTop={4}>
-   <Typography variant='h3' padding={2} textAlign={"center"}>MOVIES</Typography>
+   <Typography
+    variant='h3'
+     padding={2}
+      textAlign={"center"}
+
+      >MOVIES
+      </Typography>
+      <Box width={'100%'} margin ='auto'
+      marginTop={5}
+      display={'flex'} 
+      justifyContent="flex-start" flexWrap={"wrap"}
+      >
+{movies && movies.map((movie,index)=><MovieItem key={index} id={movie._id} posterUrl={movie.posterUrl} releaseDate={movie.releaseDate} title={movie.title}/>)}
+      </Box>
     </Box>
   )
 }
