62b16c9374c2575968a714598456412fb49bfe06
Swapnita Singh <swapimamuni@gmail.com>
2024-06-08 21:15:22 +0530

    oofff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Homepage from './components/Homepage'
@@ -8,14 +8,24 @@ import Auth from './pages/Auth'
 import Nav from './components/Nav'
 import Newnavbar from './components/Newnavbar'
 import Navbar from './components/Navbar'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { adminActions, userActions } from './store'
 
 const App = () => {
 
+  const dispatch = useDispatch();
   const isAdminLoggedIn = useSelector((state)=>state.admin.isLoggedIn);
   const isUserLoggedIn = useSelector((state)=>state.user.isLoggedIn);
 console.log("isAdminLoggedIn",isAdminLoggedIn);
 console.log("isUserLoggedIn",isUserLoggedIn);
+useEffect(()=>{ 
+  if(localStorage.getItem("userId")){
+dispatch(userActions.login());
+  }else if(localStorage.getItem("adminId")){
+dispatch(adminActions.login());
+  }
+},[])
+
 
   return (
     
