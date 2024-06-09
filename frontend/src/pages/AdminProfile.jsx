8a22d76f6a4c85a49cdb05f205ee025188053d7d
Swapnita Singh <swapimamuni@gmail.com>
2024-06-09 18:21:25 +0530

    almost

--- frontend/src/pages/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile.jsx
@@ -1,18 +1,34 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import {  getAdminById} from '../api-helpers/api-helpers'
+import {  getAdminById, getMovieDetails} from '../api-helpers/api-helpers'
 import { Box,  List, ListItem, ListItemText, Typography } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
 
 
 const AdminProfile = () => {
     const[admin,setAdmin]= useState();
-   
+    const [movies, setMovies] = useState([]);
+
     useEffect(()=>{
 getAdminById()
-.then((res)=>setAdmin(res.admin))
+.then((res)=>{
+    console.log(res);
+    console.log(res.admin.addedMovies);
+    setAdmin(res.admin)
+})
 .catch(err=>console.log(err));
  },[])
   
+
+ useEffect(() => {
+    if (admin && admin.addedMovies.length > 0) {
+      const fetchMovies = async () => {
+        const movieDetails = await Promise.all(admin.addedMovies.map((movieId) => getMovieDetails(movieId)));
+        setMovies(movieDetails);
+      };
+
+      fetchMovies();
+    }
+  }, [admin]);
 
   return(
 
@@ -36,12 +52,11 @@ borderRadius={6}
    Email: {admin.email} 
     {/* Email: {booking.user ? booking.user.email : 'Unknown'}*/ }
 </Typography>
-
-
 </Box>
 )}
 
-{admin && admin.addedMovies.length>0 &&
+{/*{admin && admin.addedMovies.length>0 &&*/}
+{movies.length > 0 &&
 (
 <Box width={'70%'}
 display={"flex"}
@@ -58,8 +73,11 @@ flexDirection={"column"}
 width={"80%"}
 >
 <List>
-  {
-    admin.addedMovies.map((movie,index)=>(
+  {/* {
+    // admin.addedMovies.map((movie,index)=>(} */}
+        {movies.map((movie, index) =>{
+            console.log(movie);
+        return (
       <ListItem
       sx={{bgcolor:"#00d386",
         color:"white",
@@ -73,12 +91,13 @@ sx={{margin:1,
   textAlign:"left"
 }}
 >
-Movie: {movie.title}
+{console.log(movie)}
+Movie: {movie.movie.title}
 </ListItemText>
 
       </ListItem>
-    ))
-  }
+    );
+        })}
 </List>
   </Box>
 </Box>
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react'
-import { deleteBooking, getUserBooking, getUserDetails } from '../api-helpers/api-helpers'
+import { deleteBooking, getMovieDetails, getUserBooking, getUserDetails } from '../api-helpers/api-helpers'
 import { Box, IconButton, List, ListItem, ListItemText, Typography } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -8,7 +8,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 const Profile = () => {
     const[bookings,setBookings]=useState([]);
    const[user,setUser]= useState();
-   
+   const [movies, setMovies] = useState({});
+
+
     useEffect(()=>{
     getUserBooking()
     .then((res)=> 
@@ -18,9 +20,30 @@ const Profile = () => {
 getUserDetails()
 .then((res)=>setUser(res.user))
 .catch(err=>console.log(err));
- },[])
-  
 
+
+const fetchMovies = async () => {
+  const movieIds = bookings.map(booking => booking.movie);
+  console.log("Movie IDs:", movieIds);
+  const movieDetails = await Promise.all(movieIds.map(movieId => getMovieDetails(movieId)));
+  console.log("Movie Details:", movieDetails);
+  
+  // const moviesObject = movieDetails.reduce((acc, movie) => {
+  //   acc[movie._id] = movie;
+  //   return acc;
+  // }, {});
+
+  const moviesObject = {};
+  movieDetails.forEach(movie => {
+    moviesObject[movie._id] = movie;
+  });
+
+  console.log("Movies Object:", moviesObject);
+  setMovies(moviesObject);
+};
+
+fetchMovies();
+}, [bookings]);
 
 
  const handleDelete= (id)=>{
@@ -48,8 +71,7 @@ border={'1px solid #ccc'}
 borderRadius={6}
 >
   Name: {user.name} 
-   {/*Name: {booking.user ? booking.user.name : 'Unknown'}*/}
-</Typography>
+  </Typography>
 
 <Typography padding={1} width={'auto'}
 textAlign={'center'}
@@ -57,8 +79,7 @@ border={'1px solid #ccc'}
 borderRadius={6}
 >
    Email: {user.email} 
-    {/* Email: {booking.user ? booking.user.email : 'Unknown'}*/ }
-</Typography>
+   </Typography>
 
 
 </Box>
@@ -82,13 +103,16 @@ width={"80%"}
 >
 <List>
   {
-    bookings.map((booking,index)=>(
+    bookings.map((booking,index)=>{
+      const movie = movies[booking.movie];
+      
+      return (
       <ListItem
       sx={{bgcolor:"#00d386",
         color:"white",
         textAlign:"center",
         margin:1,
-      }}
+      }} key={booking._id}
       >
 <ListItemText
 sx={{margin:1,
@@ -96,7 +120,9 @@ sx={{margin:1,
   textAlign:"left"
 }}
 >
-Movie: {booking.title}
+
+ {/* Movie: {booking.movie.title}  */}
+ Movie: {movie ? movie.title : "Unknown"} 
 </ListItemText>
 
 <ListItemText
@@ -125,8 +151,8 @@ Date: {new Date(booking.date).toDateString()}
 </ListItemText>
 
       </ListItem>
-    ))
-  }
+    );
+  })}
 </List>
   </Box>
 </Box>
